5311416f3dcf3e627e96d4998b0e041d1c2fa930
Eason <Eason9630@gmail.com>
2023-03-26 20:19:52 +0800

    移動圖片 Slider

--- PictureClose/ViewController.swift
+++ PictureClose/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var 達爾特: UIImageView!
+    @IBOutlet weak var 悟空: UIImageView!
+    @IBOutlet weak var 達爾: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
+    @IBAction func fussion(_ sender: UISlider) {
+        let speed = sender.value
+        
+        悟空.frame.origin.x  = CGFloat(85 + speed)
+        達爾.frame.origin.x = CGFloat(526 - speed)
+        if(speed == 120){
+            悟空.alpha = CGFloat(0)
+            達爾.alpha = CGFloat(0)
+            達爾特.alpha = CGFloat(1)
+        }else{
+            悟空.alpha = CGFloat(1)
+            達爾.alpha = CGFloat(1)
+            達爾特.alpha = CGFloat(0)
+        }
+
+        
+    }
 }
 
